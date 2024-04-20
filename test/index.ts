cb7ba6e7f2dcbb4cefe45cec8b5bbea2694ac7dc
AliBasicCoder <aliahmedreda34@gmail.com>
2024-04-21 01:00:24 +0200

    remove test

--- test/index.ts
+++ test/index.ts
@@ -6,6 +6,7 @@ import {
   expressRouter,
   httpRouter,
   init,
+  noFn,
   returns,
 } from "../src";
 import { FsFilesystem } from "../src/dir_filesystem_driver";
@@ -13,6 +14,7 @@ import { MongoDBDriver } from "../src/mongodb_driver";
 import {
   array,
   boolean,
+  date,
   file,
   id,
   number,
@@ -25,56 +27,82 @@ import { AuthorizeFunction } from "../src/types";
 import express, { Router } from "express";
 import { webCollectionsCreator } from "../src/client_generator";
 
-function validateSome(s: string) {
-  console.log("hi");
-  return true;
-}
-
 class Some extends Model {
   _id = id();
-  file = file({ noSend: true });
-  tuple = tuple({}, string(), boolean());
+  createAt = date({ noReceive: true, skip: true });
+  updateAt = date({ noReceive: true, skip: true });
 
-  static authorize = (async (req, res) => {
-    return true;
-  }) as AuthorizeFunction;
-}
+  static preUpdate<T extends typeof Model>(
+    this: T,
+    newObject: noFn<InstanceType<T>>
+  ): void {
+    // @ts-ignore
+    newObject.updateAt = new Date();
+  }
 
-class Some2 extends Model {
-  h1 = string();
-  h2 = array(string());
-  h3 = object({ h4: array(object({ h5: string() })) });
-
-  @allow(Some)
-  @returns(string())
-  @GET(string(), number())
-  static editFile(user: Some, name: string, age: number, ctx: any) {
-    console.log(name, age, ctx);
+  static preCreate<T extends typeof Model>(
+    this: T,
+    object: noFn<InstanceType<T>>
+  ): void {
+    console.log(object);
+    // @ts-ignore
+    object.createAt = new Date();
+    // @ts-ignore
+    object.updateAt = new Date();
   }
 }
 
-Some2.rule_list();
 Some.rule_create();
-Some2.rule_delete(Some);
 Some.rule_update();
+Some.rule_delete();
+Some.rule_list();
+
+// class Some extends Model {
+//   _id = id();
+//   file = file({ noSend: true });
+//   tuple = tuple({}, string(), boolean());
+
+//   static authorize = (async (req, res) => {
+//     return true;
+//   }) as AuthorizeFunction;
+// }
+
+// class Some2 extends Model {
+//   h1 = string();
+//   h2 = array(string());
+//   h3 = object({ h4: array(object({ h5: string() })) });
+
+//   @allow(Some)
+//   @returns(string())
+//   @GET(string(), number())
+//   static editFile(user: Some, name: string, age: number, ctx: any) {
+//     console.log(name, age, ctx);
+//   }
+// }
+
+// Some2.rule_list();
+// Some.rule_create();
+// Some2.rule_delete(Some);
+// Some.rule_update();
 
 async function main() {
+  const models = [Some];
+
   const router = (await init({
-    models: [Some2, Some],
+    models,
     router: expressRouter(4321),
     defaultDriver: new MongoDBDriver(
       "mongodb://localhost:27017",
       "server-db-test"
     ),
     defaultFilesystem: new FsFilesystem(path.join(__dirname, "files")),
+    outputBundle: path.join(__dirname, "../../dist/test/__LOCO_BUILD.js"),
   })) as Router;
 
   const app = express();
   app.use(router);
 
   app.listen(4321, () => console.log("Started on port 4321"));
-
-  webCollectionsCreator("/home/aliahmed/web/loco/client", [Some, Some2]);
 }
 
 main();
