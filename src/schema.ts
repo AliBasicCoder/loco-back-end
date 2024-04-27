7816f25b1aba068557de051f3d3e1a5fb634a629
AliBasicCoder <aliahmedreda34@gmail.com>
2024-04-27 22:38:59 +0300

    fix nullable type issue

--- src/schema.ts
+++ src/schema.ts
@@ -27,8 +27,14 @@ export const boolean = (options?: BooleanTypeOptions) =>
     ? ({ type: "BOOLEAN", ...options } as BooleanType)
     : undefined) as unknown as boolean;
 
-export const number = (options?: NumberTypeOptions) =>
-  (STATE.ret
+export function number(
+  options: NumberTypeOptions & { nullable: true }
+): number | null;
+export function number(
+  options?: NumberTypeOptions & { nullable?: false }
+): number;
+export function number(options?: NumberTypeOptions) {
+  return (STATE.ret
     ? ({
         type: "NUMBER",
         integerOnly: false,
@@ -36,16 +42,24 @@ export const number = (options?: NumberTypeOptions) =>
         signed: true,
         ...options,
       } as NumberType)
-    : undefined) as unknown as number;
+    : undefined) as unknown as number | null;
+}
 
-export const string = (options?: StringTypeOptions) =>
-  (STATE.ret
+export function string(
+  options: StringTypeOptions & { nullable: true }
+): string | null;
+export function string(
+  options?: StringTypeOptions & { nullable?: false }
+): string;
+export function string(options?: StringTypeOptions) {
+  return (STATE.ret
     ? ({
         type: "STRING",
         nullable: false,
         ...options,
       } as StringType)
-    : undefined) as unknown as string;
+    : undefined) as unknown as string | null;
+}
 
 export const id = () =>
   (STATE.ret
@@ -56,14 +70,19 @@ export const id = () =>
       } as StringType)
     : undefined) as unknown as string;
 
-export const date = (options?: DateTypeOptions) =>
-  (STATE.ret
+export function date(
+  options: DateTypeOptions & { nullable: true }
+): Date | null;
+export function date(options?: DateTypeOptions & { nullable?: false }): Date;
+export function date(options?: DateTypeOptions) {
+  return (STATE.ret
     ? ({
         type: "DATE",
         store: "DATE_TIME",
         ...options,
       } as DateType)
     : undefined) as unknown as Date;
+}
 
 export const ref = (to: typeof Model, options?: ReferenceTypeOptions) =>
   (STATE.ret
@@ -76,10 +95,17 @@ export const ref = (to: typeof Model, options?: ReferenceTypeOptions) =>
 
 // TODO make a File type and make Model.create
 // TODO accept Blobs on creation
-export const file = (options?: FileTypeOptions) =>
-  (STATE.ret
+export function file(
+  options: FileTypeOptions & { nullable: true }
+): string | Blob | null;
+export function file(
+  options?: FileTypeOptions & { nullable?: false }
+): string | Blob;
+export function file(options?: FileTypeOptions) {
+  return (STATE.ret
     ? ({ type: "FILE", ...options } as FileType)
-    : undefined) as unknown as string | Blob;
+    : undefined) as unknown as string | Blob | null;
+}
 
 export function array<T>(sub_type: T, options?: ArrayTypeOptions) {
   if (!STATE.ret) return undefined as unknown as T[];
@@ -98,9 +124,17 @@ export function tuple<T extends any[]>(
 
 export function object<T extends { [key: string]: any }>(
   schema: T,
+  options: ObjectTypeOptions & { nullable: true }
+): T | null;
+export function object<T extends { [key: string]: any }>(
+  schema: T,
+  options?: ObjectTypeOptions & { nullable?: false }
+): T;
+export function object<T extends { [key: string]: any }>(
+  schema: T,
   options?: ObjectTypeOptions
 ) {
   return (STATE.ret
     ? ({ type: "OBJECT", schema, ...options } as ObjectType)
-    : undefined) as unknown as T;
+    : undefined) as unknown as T | null;
 }
